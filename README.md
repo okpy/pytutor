32ee08c0a3d01ecead4d11fe6a2720220f5d336f
Sumukh Sridhara <sumukh1@gmail.com>
2017-05-06 22:52:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pytutor
+
+A library for Phillip Guo's Python Tutor
+
+
+# License
+
+MIT License
+
+
+# Credits:
+
+Phillip Guo - Python Tutor
+Sumukh Sridhara
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='pytutor',
+  packages=['pytutor'], # this must be the same as the name above
+  version='0.1',
+  description='An interface to run Phillip Guo\'s Python Tutor',
+  author='Sumukh Sridhara and Phillip Guo',
+  author_email='sumukh@berkeley.edu',
+  url='https://github.com/okpy/pytutor', # use the URL to the github repo
+  download_url='https://github.com/okpy/pytutor/archive/0.1.tar.gz',
+  keywords=['tracing', 'education', 'okpy', 'python tutor'], # arbitrary keywords
+  classifiers=[],
+)
