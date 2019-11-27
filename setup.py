2c714dc854e9e5e39c28da0368a9af705ab79b51
Sumukh Sridhara <sumukh1@gmail.com>
2019-11-26 16:36:30 -0800

    1.0.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='pytutor',
   packages=['pytutor'], # this must be the same as the name above
-  version='0.1',
+  version='1.0.0',
   description='An interface to run Phillip Guo\'s Python Tutor',
-  author='Sumukh Sridhara and Phillip Guo',
+  author='Sumukh Sridhara',
   author_email='sumukh@berkeley.edu',
   url='https://github.com/okpy/pytutor', # use the URL to the github repo
-  download_url='https://github.com/okpy/pytutor/archive/0.1.tar.gz',
-  keywords=['tracing', 'education', 'okpy', 'python tutor'], # arbitrary keywords
+  download_url='https://github.com/okpy/pytutor/archive/1.0.0.tar.gz',
+  keywords=['tracing', 'education', 'okpy', 'tutor', 'debugger', 'python tutor'], # arbitrary keywords
   classifiers=[],
 )
