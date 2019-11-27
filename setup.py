62a17735a3a8ccfeb72d6025a1025f8aa7ad2162
Sumukh Sridhara <sumukh1@gmail.com>
2019-11-26 16:38:44 -0800

    add classifiers

--- setup.py
+++ setup.py
@@ -9,5 +9,9 @@ setup(
   url='https://github.com/okpy/pytutor', # use the URL to the github repo
   download_url='https://github.com/okpy/pytutor/archive/1.0.0.tar.gz',
   keywords=['tracing', 'education', 'okpy', 'tutor', 'debugger', 'python tutor'], # arbitrary keywords
-  classifiers=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
 )
